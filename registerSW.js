bfbd9eb76023c65b531e68d32db6bedef5cfb631
Danny <dgr73@cornell.edu>
2023-01-05 17:25:10 -0500

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/vue-ggies-tales-project-main/sw.js', { scope: '/vue-ggies-tales-project-main/' })})}
