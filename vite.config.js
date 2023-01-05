e738ecbb7ce151bd2740e17a24086ede01657b91
Danny <dgr73@cornell.edu>
2023-01-05 16:55:17 -0500

    structural

--- vite.config.js
+++ vite.config.js
@@ -1,49 +1,9 @@
-import { fileURLToPath, URL } from 'node:url'
-import { VitePWA } from 'vite-plugin-pwa'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "node:url";
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/info4340-2022fa/vue-ggie-tales-project/' : './',
-  plugins: [vue(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['logo.png', 'masked-icon.svg'],
-      manifest: {
-        name: 'Vue-ggie Tales',
-        short_name: 'Vue-ggieTales',
-        description: 'Vue-ggie Tales prototype for INFO 4340',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'logo.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'logo.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'logo.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-      },
-      devOptions: {
-        enabled: true
-      }
-    })],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+  plugins: [vue()],
+});
